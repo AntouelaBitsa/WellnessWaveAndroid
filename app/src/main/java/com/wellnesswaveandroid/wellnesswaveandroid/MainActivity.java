071e81c80dac0e2c4d4c4bc604bc8e50aa3584d6
AntouelaBitsa <antoubi10@gmail.com>
2024-03-24 01:38:18 +0200

    Adding Icons & rounded_background for textfield
    Adding icons that will be used in my application
    Creating a rounded background for the text fields

--- app/src/main/java/com/wellnesswaveandroid/wellnesswaveandroid/MainActivity.java
+++ app/src/main/java/com/wellnesswaveandroid/wellnesswaveandroid/MainActivity.java
@@ -9,8 +9,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-//        setContentView(R.layout.activity_log_in);
-        setContentView(R.layout.activity_user_type);
+        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_log_in);
+//        setContentView(R.layout.activity_user_type);
     }
 }
