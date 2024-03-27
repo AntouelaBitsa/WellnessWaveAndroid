efbd8687a052c2df3fa584f4dbbaf60fcb41cf82
AntouelaBitsa <antoubi10@gmail.com>
2024-03-27 11:52:48 +0200

    Added Functionality to Log In Activity
    Added onClick Listener for the Sign in Button from Log in Activity to User Type Activity with Intent

--- app/src/main/java/com/wellnesswaveandroid/wellnesswaveandroid/Activities/LogInActivity.java
+++ app/src/main/java/com/wellnesswaveandroid/wellnesswaveandroid/Activities/LogInActivity.java
@@ -2,15 +2,31 @@ package com.wellnesswaveandroid.wellnesswaveandroid.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.wellnesswaveandroid.wellnesswaveandroid.R;
 
 public class LogInActivity extends AppCompatActivity {
 
+    private Button logInButton, signInButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+
+        //TODO : implementation Log In Button
+
+        signInButton = findViewById(R.id.signInBtn);
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent userType = new Intent(LogInActivity.this, UserTypeActivity.class);
+                startActivity(userType);
+            }
+        });
     }
 }
