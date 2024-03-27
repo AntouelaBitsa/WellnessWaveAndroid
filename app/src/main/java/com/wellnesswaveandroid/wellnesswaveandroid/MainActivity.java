230a7b80a6817be6e01b7bc86b693ae9b97dfe2a
AntouelaBitsa <antoubi10@gmail.com>
2024-03-27 11:41:08 +0200

    Added Functionality to Main Activity
    Added onClick Listener from Main Activity to Log In Activity with Intent.
    Fixed errors in the xml file of activity_doc_details.

--- app/src/main/java/com/wellnesswaveandroid/wellnesswaveandroid/MainActivity.java
+++ app/src/main/java/com/wellnesswaveandroid/wellnesswaveandroid/MainActivity.java
@@ -2,19 +2,34 @@ package com.wellnesswaveandroid.wellnesswaveandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button enter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+        //For testing puposes
 //        setContentView(R.layout.activity_log_in);
 //        setContentView(R.layout.activity_user_type);
 //        setContentView(R.layout.activity_doc_sign_in);
 //        setContentView(R.layout.activity_doc_details);
 //        setContentView(R.layout.activity_pat_sign_in);
-        setContentView(R.layout.activity_pat_details);
+//        setContentView(R.layout.activity_pat_details);
+
+        enter = findViewById(R.id.enterBtn);
+        enter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent enterApplication = new Intent(MainActivity.this, LogInActivity.class);
+                startActivity(enterApplication);
+            }
+        });
     }
 }
