5e49cd32078f18f1eddf020d2fe94927c971a23c
AntouelaBitsa <antoubi10@gmail.com>
2024-05-16 11:35:55 +0300

    Updating DoctorApi Interface
    Inserting Declaration of the Endpoint getDoctorById in the DoctorApi interface

--- app/src/main/java/com/wellnesswaveandroid/wellnesswaveandroid/Retrofit/DoctorApi.java
+++ app/src/main/java/com/wellnesswaveandroid/wellnesswaveandroid/Retrofit/DoctorApi.java
@@ -4,9 +4,15 @@ import com.wellnesswaveandroid.wellnesswaveandroid.Entities.Doctor;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.Path;
 
 public interface DoctorApi {
     @POST("/createDoctor")
     Call<Doctor> createDoctor(@Body Doctor doctor);
+
+    //DONE: get endpoint statement
+    @GET("/getDoctorById/{id}")
+    Call<Doctor> getDoctorById(@Path("id") Integer id);
 }
