ac3dd2c6ff5c98759b7a5b9ce027634b486eed99
AntouelaBitsa <antoubi10@gmail.com>
2024-07-23 00:02:37 +0300

    Adding Endpoint Declaration & Helper Methods

--- app/src/main/java/com/wellnesswaveandroid/wellnesswaveandroid/Entities/Patient.java
+++ app/src/main/java/com/wellnesswaveandroid/wellnesswaveandroid/Entities/Patient.java
@@ -23,8 +23,42 @@ public class Patient {
     @SerializedName("dob")
     private String patDob;
     private int userType;
+    //Transfer patient data between activities
+    private static Patient patInstance;
 
-    public Patient() {
+    //Transfer patient data between activities
+    public static synchronized Patient getInstance(){
+        if (patInstance == null){
+            patInstance = new Patient();
+        }
+        return patInstance;
+    }
+
+    //Transfer patient data between activities
+    public void setPatientData(Patient p){
+        this.patientId = p.getPatientId();
+        this.patFirstName = p.getPatFirstName();
+        this.patLastName = p.getPatLastName();
+        this.patUsername = p.getPatUsername();
+        this.patEmail = p.getPatEmail();
+        this.patPhoneNum = p.getPatPhoneNum();
+        this.patDob = p.getPatDob();
+        this.userType = p.getUserType();
+    }
+
+    //This constructor must be private fot the data transfer
+    private Patient() {
+    }
+
+    public Patient(Integer patientId, String patFirstName, String patLastName, String patUsername, String patEmail, String patPhoneNum, String patDob, int userType) {
+        this.patientId = patientId;
+        this.patFirstName = patFirstName;
+        this.patLastName = patLastName;
+        this.patUsername = patUsername;
+        this.patEmail = patEmail;
+        this.patPhoneNum = patPhoneNum;
+        this.patDob = patDob;
+        this.userType = userType;
     }
 
     public Patient(String patFirstName, String patLastName, String patUsername, String patPassword, String patEmail, String patPhoneNum, String patSecuredNum, String patDob, int userType) {
@@ -37,6 +71,10 @@ public class Patient {
         this.patSecuredNum = patSecuredNum;
         this.patDob = patDob;
         this.userType = userType;
+    }
+
+    public Patient(Integer bookPat) {
+        this.patientId = bookPat;
     }
 
     public Integer getPatientId() {
--- app/src/main/java/com/wellnesswaveandroid/wellnesswaveandroid/Retrofit/PatientApi.java
+++ app/src/main/java/com/wellnesswaveandroid/wellnesswaveandroid/Retrofit/PatientApi.java
@@ -1,9 +1,12 @@
 package com.wellnesswaveandroid.wellnesswaveandroid.Retrofit;
 
 import com.wellnesswaveandroid.wellnesswaveandroid.Entities.Patient;
+import com.wellnesswaveandroid.wellnesswaveandroid.Utils.Result;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.POST;
 
 public interface PatientApi {
@@ -13,4 +16,7 @@ public interface PatientApi {
 
     //TODO: Get andpoint for getPatientById
     //TODO: logIn endpoint
+    @FormUrlEncoded
+    @POST("/patientLogInSession")
+    Call<Result> logIn(@Field("username") String username, @Field("password") String password);
 }
