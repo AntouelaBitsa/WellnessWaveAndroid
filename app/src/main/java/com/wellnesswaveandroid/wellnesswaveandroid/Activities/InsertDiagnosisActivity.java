0759f69715b102cc26e8237592e1d59b12aa2bec
AntouelaBitsa <antoubi10@gmail.com>
2024-07-23 00:11:29 +0300

    Creating Insert Diagnosis Session Class & Activity

---
+++ app/src/main/java/com/wellnesswaveandroid/wellnesswaveandroid/Activities/InsertDiagnosisActivity.java
@@ -0,0 +1,16 @@
+package com.wellnesswaveandroid.wellnesswaveandroid.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.wellnesswaveandroid.wellnesswaveandroid.R;
+
+public class InsertDiagnosisActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insert_diagnosis);
+    }
+}
