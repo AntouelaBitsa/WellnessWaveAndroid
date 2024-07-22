e32222a55b033e6f26aad0a9c29e809b8289525f
AntouelaBitsa <antoubi10@gmail.com>
2024-07-23 00:10:59 +0300

    Creating Doctor Home Session Class & Activity

---
+++ app/src/main/java/com/wellnesswaveandroid/wellnesswaveandroid/Activities/DoctorHomeActivity.java
@@ -0,0 +1,16 @@
+package com.wellnesswaveandroid.wellnesswaveandroid.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.wellnesswaveandroid.wellnesswaveandroid.R;
+
+public class DoctorHomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doctor_home);
+    }
+}
