f14a77c7df31068255192ae06772ec74d4ace264
AntouelaBitsa <antoubi10@gmail.com>
2024-12-11 21:41:10 +0200

    UI Updates on Doctor Details Activity
    Regined design based on FIgma designs

--- app/src/main/java/com/wellnesswaveandroid/wellnesswaveandroid/Activities/DocDetails.java
+++ app/src/main/java/com/wellnesswaveandroid/wellnesswaveandroid/Activities/DocDetails.java
@@ -1,54 +1,86 @@
 package com.wellnesswaveandroid.wellnesswaveandroid.Activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
+import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+import com.google.android.material.textfield.TextInputEditText;
 import com.wellnesswaveandroid.wellnesswaveandroid.Entities.Doctor;
 import com.wellnesswaveandroid.wellnesswaveandroid.R;
-import com.wellnesswaveandroid.wellnesswaveandroid.Retrofit.DoctorApi;
 import com.wellnesswaveandroid.wellnesswaveandroid.Retrofit.RetrofitService;
 
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
+import java.util.Map;
 
 public class DocDetails extends AppCompatActivity {
-    //TODO: data arent showing into my Activity must check for it
-    TextView firstNameTxt, lastNameTxt, usernameTxt, passwordTxt, emailTxt, amkaTxt, phoneNumTxt, professionTxt, addressTxt;
+    //DONE: data arent showing into my Activity must check for it
+    private BottomNavigationView bottomNavigationView;
+    TextView fullNameTxt, lastNameTxt, usernameTxt;
+    private TextInputEditText passwordTxt, emailTxt, amkaTxt, phoneNumTxt, professionTxt, addressTxt;
+    private ImageView deleteDocAccount;
+    private Button editDocProfile;
     RetrofitService retrofitService;
-    private Button btnTest;
     private static Integer docID;
-
+    private Doctor docInstance;
+    private boolean btnState = false;
+    private Map<String, Object> updatedFields;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doc_details);
 
+        bottomNavigationView = findViewById(R.id.bottomNavBar);
+        bottomNavigationView.setSelectedItemId(R.id.nav_profile);
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId() == R.id.nav_home) {
+                    startActivity(new Intent(getApplicationContext(), DoctorHomePageActivity.class));
+                    //if there are transitions=> overridePendingTransition()
+                    finish();
+                    return true;
+                }else if (item.getItemId() == R.id.nav_profile) {
+                    return true;
+                } else if (item.getItemId() == R.id.nav_manage_appointments) {
+//                        startActivity(new Intent(getApplicationContext() /*, Manage Appointments class */));
+                    //if there are transitions=> overridePendingTransition()
+//                        finish();
+                    return true;
+                }else {
+                    return false;
+                }
+            }
+        });
+
         //initialization: find views by id for edit texts
-        firstNameTxt = findViewById(R.id.docFNameTxt);
-        lastNameTxt = findViewById(R.id.docLNameTxt);
+        fullNameTxt = findViewById(R.id.docFullNameTxt);
         usernameTxt = findViewById(R.id.docUsernameTxt);
-        passwordTxt = findViewById(R.id.docPasswordTxt);
+//        passwordTxt = findViewById(R.id.docPasswordTxt);
         emailTxt = findViewById(R.id.docEmailTxt);
-        amkaTxt = findViewById(R.id.docAmkaTxt);
+//        amkaTxt = findViewById(R.id.docAmkaTxt);
         phoneNumTxt = findViewById(R.id.docPhoneNumTxt);
         professionTxt = findViewById(R.id.docProfessionTxt);
         addressTxt = findViewById(R.id.docAddressTxt);
+        editDocProfile = findViewById(R.id.editDocProfileBtn);
+        deleteDocAccount = findViewById(R.id.deleteDocAccount);
 
         Log.d("[D] Are HEre", " Yes");
         Toast.makeText(DocDetails.this, "Intent worked Successfully", Toast.LENGTH_LONG).show();
 
         Intent importedDocData = getIntent();
         docID = importedDocData.getIntExtra("doc_id",-1); //test
-        String firstName = importedDocData.getStringExtra("first_name");
+        String fullName = importedDocData.getStringExtra("first_name");
         String lastName = importedDocData.getStringExtra("last_name");
         String username = importedDocData.getStringExtra("username");
         String email = importedDocData.getStringExtra("email");
@@ -57,7 +89,7 @@ public class DocDetails extends AppCompatActivity {
         String address = importedDocData.getStringExtra("address");
 
 //        Doctor d = Doctor.getInstance();
-//        String firstName = d.getDocFirstName();
+//        String fullName = d.getDocFirstName();
 //        String lastName = d.getDocLastName();
 //        String username = d.getDocUsername();
 //        String email = d.getDocEmail();
@@ -65,43 +97,42 @@ public class DocDetails extends AppCompatActivity {
 //        String profession = d.getDocProfession();
 //        String address = d.getDocAddress();
 
-        Doctor.getInstance().setDocId(docID);  // Ensure the ID is set
-        Doctor.getInstance().setDocFirstName(firstName);
-        Doctor.getInstance().setDocLastName(lastName);
-        Doctor.getInstance().setDocUsername(username);
-        Doctor.getInstance().setDocEmail(email);
-        Doctor.getInstance().setDocPhoneNum(phoneNum);
-        Doctor.getInstance().setDocProfession(profession);
-        Doctor.getInstance().setDocAddress(address);
+//        Doctor.getInstance().setDocId(docID);  // Ensure the ID is set
+//        Doctor.getInstance().setDocFirstName(fullName);
+//        Doctor.getInstance().setDocLastName(lastName);
+//        Doctor.getInstance().setDocUsername(username);
+//        Doctor.getInstance().setDocEmail(email);
+//        Doctor.getInstance().setDocPhoneNum(phoneNum);
+//        Doctor.getInstance().setDocProfession(profession);
+//        Doctor.getInstance().setDocAddress(address);
 
-        System.out.println("Intent Print : " + firstName+ " " +lastName+ " " +username+ " " +email+
+        docInstance = Doctor.getInstance();
+        if (docInstance == null){
+            System.out.println("The doctor instance in DOCDETAILS Class is null");
+            return;
+        }
+        String dr = "Dr.";
+        fullName = dr.concat(" ").concat(docInstance.getDocFirstName()).concat(" ").concat(docInstance.getDocLastName());
+//        lastName = docInstance.getDocLastName();
+        username = docInstance.getDocUsername();
+        email = docInstance.getDocEmail();
+        phoneNum = docInstance.getDocPhoneNum();
+        profession = docInstance.getDocProfession();
+        address = docInstance.getDocAddress();
+
+        System.out.println("Intent Print : " + fullName+ " " +lastName+ " " +username+ " " +email+
                 " " +phoneNum+ " " +profession+ " " +address);
 
-        firstNameTxt.setText(firstName);
-        lastNameTxt.setText(lastName);
+        fullNameTxt.setText(fullName);
+//        lastNameTxt.setText(lastName);
         usernameTxt.setText(username);
         emailTxt.setText(email);
         phoneNumTxt.setText(phoneNum);
         professionTxt.setText(profession);
         addressTxt.setText(address);
 
-        //TEST BUTTON
-        btnTest = findViewById(R.id.btnTestDoc);
-        btnTest.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(DocDetails.this, InsertDiagnosisActivity.class);
-//                intent.putExtra("doc_id", docID);
-                startActivity(intent);
-            }
-        });
-
-//        callGetRequestForDetails();
-        //TEST INTENT FROM MAIN
-//        intentGetMethod("Antouela");
+        //TODO: EDIT PROFILE LISTENER & CALL REQUEST
+        //TODO: DELETE PROFILE LISTENER & CALL REQUEST
     }
 
-    private Integer returnDocID(Integer id){
-        return docID;
-    }
 }
