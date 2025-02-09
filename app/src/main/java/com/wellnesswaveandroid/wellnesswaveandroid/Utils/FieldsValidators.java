62cb35c488a427efea68124e2deba6ef09ee3907
AntouelaBitsa <antoubi10@gmail.com>
2025-02-09 16:24:34 +0200

    Updating Fields Validators

--- app/src/main/java/com/wellnesswaveandroid/wellnesswaveandroid/Utils/FieldsValidators.java
+++ app/src/main/java/com/wellnesswaveandroid/wellnesswaveandroid/Utils/FieldsValidators.java
@@ -1,5 +1,7 @@
 package com.wellnesswaveandroid.wellnesswaveandroid.Utils;
 
+import android.util.Log;
+
 import java.util.regex.Pattern;
 
 public class FieldsValidators {
@@ -7,12 +9,22 @@ public class FieldsValidators {
     public FieldsValidators() {
     }
 
-    public boolean validateEmpty(String fname, String lname, String username, String password, String email, String amka, String phone, String profession, String address){
-        return fname.isEmpty() && lname.isEmpty() && username.isEmpty() && password.isEmpty() && email.isEmpty() && amka.isEmpty()
-                && phone.isEmpty() && profession.isEmpty() && address.isEmpty();
+    public boolean validateEmptyDoc(String fname, String lname, String username, String password, String email, String amka, String phone, String profession, String address){
+        return fname.isEmpty() || lname.isEmpty() || username.isEmpty() || password.isEmpty() || email.isEmpty() || amka.isEmpty()
+                || phone.isEmpty() || profession.isEmpty() || address.isEmpty();
     }
 
-    public boolean validateRegex(String password, String email, String amka, String phone){
+    public boolean validateEmptyPat(String fname, String lname, String username, String password, String email, String amka, String phone, String dob){
+        return fname.isEmpty() || lname.isEmpty() || username.isEmpty() || password.isEmpty() || email.isEmpty() || amka.isEmpty()
+                || phone.isEmpty() || dob.isEmpty();
+    }
+
+    public boolean validateRegex(String password, String email, String phone, String amka){
+        Log.d("InputCheck", "Password: " + password);
+        Log.d("InputCheck", "Email: " + email);
+        Log.d("InputCheck", "AMKA: " + amka);
+        Log.d("InputCheck", "Phone: " + phone);
+
         String passRegex = "^(?=.*[0-9])"            //a digit must occur at least once
                 + "(?=.*[a-z])(?=.*[A-Z])"      //a lower case alphabet must occur at least once & an upper case alphabet that must occur at least once
                 + "(?=.*[@!^&_])"               //a special character that must occur at least once
@@ -24,27 +36,82 @@ public class FieldsValidators {
                 + "+\\.[a-zA-Z]{2,}$";
         Pattern emailPattern = Pattern.compile(emailRegex);
 
-        String phoneNumber = "^\\+[1-9\\d{1,10}$]";
+//        String phoneNumber = "^\\+?[1-9][0-9]{9,14}$";
+//        String phoneNumber = "^(\\(?\\d{3}\\)?[-.\\s]?)?\\d{3}[-.\\s]?\\d{4}$";
+        String phoneNumber = "^[0-9]{10}$";
         Pattern phonePattern = Pattern.compile(phoneNumber);
 
         String securedNum = "^[0-9]{11}$";
         Pattern securedNumPattern = Pattern.compile(securedNum);
 
-        if (!passPattern.matcher(password).matches() || !emailPattern.matcher(email).matches() ||
-                !phonePattern.matcher(phone).matches() || !securedNumPattern.matcher(amka).matches()) {
+        if (!passPattern.matcher(password).matches()) {
+            Log.d("Debug PASSWORD", "REGEX: password = " +  !passPattern.matcher(password).matches() );
+            return true;
+        }
+
+        if (!emailPattern.matcher(email).matches()){
+            Log.d("Debug EMAIL", "REGEX: email = " +  !emailPattern.matcher(email).matches() );
+            return true;
+        }
+
+        if (!phonePattern.matcher(phone).matches()){
+            Log.d("Debug PHONE", "REGEX: phone = " + !phonePattern.matcher(phone).matches() + " Phone length: " + phone.length());
+            return true;
+        }
+
+        if (!securedNumPattern.matcher(amka).matches()){
+            Log.d("Debug AMKA", "REGEX: amka = " +  !securedNumPattern.matcher(securedNum).matches());
             return true;
         }
 
         return false;
     }
 
-    public boolean validateLength(String password, String amka, String phone){
-        if (!(password.length()>=8 && password.length()<=15) || !(amka.length()==11) || !(phone.length()==10) ){
-            System.out.println("[ValUtilsClass] Password= " + !(password.length()>=8 && password.length()<=15) +
-                    " AMKA= " + !(amka.length()==11) + " Phone= " + !(phone.length()==10));
+    public boolean validateLength(String password, String phone, String amka){
+        if ( !(password.length()>=8 && password.length()<=15) ){
+            System.out.println("[ValUtilsClass] Password= " + !(password.length()>=8 && password.length()<=15));
+            Log.d("Debug PASSWORD", "validateLength: password = " +  !(password.length()>=8 && password.length()<=15) + "pass length: " + password.length());
+            return true;
+        }
+        if (amka.length()<10 || amka.length()>12) {
+            System.out.println("[ValUtilsClass] " + " AMKA= " + !(amka.length()==11));
+            Log.d("Debug AMKA", "validateLength: amka = " +  !(amka.length()==11) + "amka length: " + amka.length());
+            return true;
+        }
+        if (phone.length()<9 || phone.length()>11) {
+            System.out.println("[ValUtilsClass] " + " Phone= " + !(phone.length()==10));
+            Log.d("Debug AMKA", "validateLength: phone = " +  !(phone.length()==10) + "phone length: " + phone.length());
             return true;
         }
 
+        return false;
+    }
+
+    public boolean validateEmptyLogIn(String username, String password) {
+        return username.isEmpty() || password.isEmpty();
+    }
+
+    public boolean validateRegexLogIn(String password) {
+        String passRegex = "^(?=.*[0-9])"            //a digit must occur at least once
+                + "(?=.*[a-z])(?=.*[A-Z])"      //a lower case alphabet must occur at least once & an upper case alphabet that must occur at least once
+                + "(?=.*[@!^&_])"               //a special character that must occur at least once
+                + "(?=\\S+$).{8,15}$";          //white spaces aren’t allowed & at least 8 characters and at most 15 characters
+        Pattern passPattern = Pattern.compile(passRegex);
+
+        if (!passPattern.matcher(password).matches()) {
+            Log.d("Debug PASSWORD", "REGEX: password = " +  !passPattern.matcher(password).matches() );
+            return true;
+        }
+
+        return false;
+    }
+
+    public boolean validateLengthLogIn(String password) {
+        if ( !(password.length()>=8 && password.length()<=15) ){
+            System.out.println("[ValUtilsClass] Password= " + !(password.length()>=8 && password.length()<=15));
+            Log.d("Debug PASSWORD", "validateLength: password = " +  !(password.length()>=8 && password.length()<=15) + "pass length: " + password.length());
+            return true;
+        }
         return false;
     }
 
@@ -86,7 +153,8 @@ public class FieldsValidators {
     }
 
     public String validatePhoneNumber(String incomingPhoneNumber){
-//        String phoneNumber = "^\\+[1-9\\d{1,10}$]";
+//        String phoneNumber = "^\\+?[1-9][0-9]{9,14}$";
+//        String phoneNumber = "^(\\(?\\d{3}\\)?[-.\\s]?)?\\d{3}[-.\\s]?\\d{4}$";
         String phoneNumber = "^[0-9]{10}$";
         Pattern phonePattern = Pattern.compile(phoneNumber);
 
@@ -151,4 +219,14 @@ public class FieldsValidators {
             return "";
         }
     }
+
+    public String validateDob(String incomingDob) {
+        if (incomingDob.isEmpty()){
+            return "Birthdate is required.";
+        }else {
+            return "";
+        }
+    }
+
+
 }
