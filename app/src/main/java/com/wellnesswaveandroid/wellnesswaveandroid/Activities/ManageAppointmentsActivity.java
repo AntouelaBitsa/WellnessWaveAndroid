f0c40b5274ed2a98c1940263e7766c3543272ee1
AntouelaBitsa <antoubi10@gmail.com>
2025-02-09 16:54:47 +0200

    Updates on Manage Appointments Activity: Compliance for Both Users
    Updating functionality to manage appointments for both Patient and Doctor

--- app/src/main/java/com/wellnesswaveandroid/wellnesswaveandroid/Activities/ManageAppointmentsActivity.java
+++ app/src/main/java/com/wellnesswaveandroid/wellnesswaveandroid/Activities/ManageAppointmentsActivity.java
@@ -11,33 +11,25 @@ import android.widget.CalendarView;
 import android.widget.DatePicker;
 import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.datepicker.MaterialDatePicker;
-import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener;
 import com.google.android.material.navigation.NavigationBarView;
 import com.wellnesswaveandroid.wellnesswaveandroid.Entities.Appointments;
+import com.wellnesswaveandroid.wellnesswaveandroid.Entities.Diagnosis;
 import com.wellnesswaveandroid.wellnesswaveandroid.Entities.Doctor;
+import com.wellnesswaveandroid.wellnesswaveandroid.Entities.Patient;
 import com.wellnesswaveandroid.wellnesswaveandroid.R;
 import com.wellnesswaveandroid.wellnesswaveandroid.Retrofit.AppointmentsApi;
 import com.wellnesswaveandroid.wellnesswaveandroid.Retrofit.RetrofitService;
-import com.wellnesswaveandroid.wellnesswaveandroid.Utils.DocAppointmentCarouselAdapter;
 import com.wellnesswaveandroid.wellnesswaveandroid.Utils.DocManageAppointmentsAdapter;
+import com.wellnesswaveandroid.wellnesswaveandroid.Utils.LogInDTO;
 
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
 import java.util.List;
-import java.util.Locale;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -55,24 +47,18 @@ public class ManageAppointmentsActivity extends AppCompatActivity {
     private Button popUpYesBtn, popUpNoBtn;
 
     private Doctor docInstance;
+    private Patient patInstance;
     private Integer docID;
     private List<Appointments> appointmentsList;
+    private LogInDTO userTypeInstance;
+    private Integer userType;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manage_appointments);
-
-        //Get Doctor instance: ID
-        docInstance = Doctor.getInstance();
-        System.out.println("TEST PRINT SOS 2 DOCTOR: "+ docInstance.getDocId());
-        if (docInstance!=null){
-            System.out.println("[1 - ManageAppoint] docInstance.getDocUsername()= " + docInstance.getDocUsername());
-            System.out.println("[2 - ManageAppoint] docInstance.getDocId()= " + docInstance.getDocId());
-        }else{
-            System.out.println("[3 - ManageAppoint] docInstance == null");
-        }
-
+        
+        
         bottomNavigationView = findViewById(R.id.bottomNavBar);
         bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
@@ -81,25 +67,46 @@ public class ManageAppointmentsActivity extends AppCompatActivity {
                     startActivity(new Intent(getApplicationContext(), DoctorHomePageActivity.class));
                     finish();
                     return true;
-                }else if (item.getItemId() == R.id.nav_profile) {
-                    startActivity(new Intent(getApplicationContext(), DocDetails.class));
-                    //if there are transitions=> overridePendingTransition()
-                    finish();
-                    return true;
-                } else if (item.getItemId() == R.id.nav_manage_appointments) {
-//                    startActivity(new Intent(getApplicationContext() , ManageAppointmentsActivity.class));
-                    //if there are transitions=> overridePendingTransition()
-//                    finish();
+                }else if (item.getItemId() == R.id.nav_manage_appointments) {
                     return true;
                 }else if (item.getItemId() == R.id.nav_diagn_history){
-//                    startActivity(new Intent(getApplicationContext()/*, Diagnosis History */));
+                    startActivity(new Intent(getApplicationContext(), DiagnosisRecordsActivity.class));
 //                    finish();
+                    return true;
+                }else if (item.getItemId() == R.id.nav_profile) {
+                    startActivity(new Intent(getApplicationContext(), DocDetails.class));
+                    finish();
                     return true;
                 }else{
                     return false;
                 }
             }
         });
+
+        userTypeInstance = LogInDTO.getInstance();
+        if (userTypeInstance == null){
+            System.out.println("[Manage Appointments] userTypeInstance == NULL");
+        }
+        System.out.println("[Manage Appointments] userTypeInstance == " + userTypeInstance.getUserType());
+
+        userType = userTypeInstance.getUserType();
+        if (userType == 1){
+            docInstance = Doctor.getInstance();
+            System.out.println("TEST PRINT SOS 2 DOCTOR: "+ docInstance.getDocId());
+            if (docInstance == null){
+                System.out.println("[ManageAppoint 1] docInstance == null");
+            }
+            System.out.println("[ManageAppoint 2] docInstance.getDocUsername()= " + docInstance.getDocUsername());
+            System.out.println("[ManageAppoint 3] docInstance.getUserType()= " + docInstance.getUserType());
+        } else if (userType == 2) {
+            patInstance = Patient.getInstance();
+            System.out.println("TEST PRINT SOS 2 PATIENT: "+ patInstance.getPatientId());
+            if (patInstance == null){
+                System.out.println("[ManageAppoint 4] patInstance == null");
+            }
+            System.out.println("[ManageAppoint 5] patInstance.getPatUsername()= " + patInstance.getPatUsername());
+            System.out.println("[ManageAppoint 6] patInstance.getUserType()= " + patInstance.getUserType());
+        }
 
         appointmentsList = new ArrayList<>();
         appointReclrView = findViewById(R.id.manageRecycler);
@@ -119,10 +126,16 @@ public class ManageAppointmentsActivity extends AppCompatActivity {
             @Override
             public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
                 // Month is 0-based, so add 1
-                selectedDate = year + "-" + (month + 1) + "-" + dayOfMonth;
+//                selectedDate = year + "-" + (month + 1) + "-" + dayOfMonth;
+                selectedDate = String.format("%04d-%02d-%02d", year, month+1, dayOfMonth);
                 // Display selected date
                 Toast.makeText(ManageAppointmentsActivity.this, "Selected Date: " + selectedDate, Toast.LENGTH_SHORT).show();
-                getDateBasedAppointments(selectedDate, docInstance.getDocId());
+                if (userType == 1){
+                    getDateBasedAppointmentsDoc(selectedDate, docInstance.getDocId(), userType);
+                } else if (userType == 2) {
+                    getDateBasedAppointmentsDoc(selectedDate, patInstance.getPatientId(), userType);
+                }
+
             }
         });
 
@@ -157,14 +170,14 @@ public class ManageAppointmentsActivity extends AppCompatActivity {
         });
     }
 
-    private void getDateBasedAppointments(String selectedDate, Integer docID) {
+    private void getDateBasedAppointmentsDoc(String selectedDate, Integer userId, Integer userType) {
         //TODO: API call to endpoint
         System.out.println("Inside getDateBasedAppointments()");
-        System.out.println("[SOS PRINT] Incoming Parameters: Id= "+ docID + " and SelectedDate= " + selectedDate);
+        System.out.println("[SOS PRINT] Incoming Parameters: Id= "+ userId + " and SelectedDate= " + selectedDate + " and userType= " + userType);
         RetrofitService retrofitService = new RetrofitService();
         AppointmentsApi appointmentsApi = retrofitService.getRetrofit().create(AppointmentsApi.class);
         System.out.println("[SOS PRINT] --> before retrofit get request call");
-        appointmentsApi.getAppointmentsOnDateSelected(selectedDate, docID).enqueue(new Callback<List<Appointments>>() {
+        appointmentsApi.getAppointmentsOnDateSelected(selectedDate, userId, userType).enqueue(new Callback<List<Appointments>>() {
             @Override
             public void onResponse(Call<List<Appointments>> call, Response<List<Appointments>> response) {
                 System.out.println("[ManageAppointmentsActivity] onResponse 1 : response BODY-> " + response.body());
@@ -173,8 +186,13 @@ public class ManageAppointmentsActivity extends AppCompatActivity {
                     System.out.println("[ManageAppointmentsActivity] onResponse 2 : successful= no + body== null");
                     Toast.makeText(ManageAppointmentsActivity.this, "Failed to get doctor's appointments list", Toast.LENGTH_SHORT).show();
                     appointManageAdapter.notifyDataSetChanged();
+                    return;
                 }
 
+//                if (response.body() == null){
+//                    System.out.println("[ManageAppointmentsActivity]: response body is NULL" + response.body());
+//                    return;
+//                }
                 System.out.println("[ManageAppointmentsActivity] onResponse 3 : response BODY 2--> " + response.body());
                 appointmentsList.addAll(response.body());
                 //patUsername.setText(pUsername);
